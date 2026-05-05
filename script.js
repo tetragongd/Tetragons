23faf03a275f06ced888e88ce3765e2eb49f7cd7
tetragongd <sawrduan@gmail.com>
2026-05-05 16:27:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// 1. Grab the button element by its ID from the HTML
+const sitesButton = document.getElementById('sitesLink');
+
+// 2. Add an event listener to 'listen' for a mouse click
+sitesButton.addEventListener('click', function() {
+    
+    // 3. Log a message to the console (useful for debugging!)
+    console.log("Redirecting to the sites page...");
+
+    // 4. Change the URL of the current tab
+    window.location.href = "https://tetragon4.com/sites";
+});
